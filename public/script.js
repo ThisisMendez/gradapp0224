4f595c2920305fbabf764a911ee3b6cf8c494c16
Alexis Mendez <alexis.mendezpvd@gmail.com>
2020-02-27 14:00:07 -0500

    edit button works

--- public/script.js
+++ public/script.js
@@ -22,13 +22,16 @@ const renderGrads = () => {
     const gradList = grads.map(element => { 
         return ( 
             "<li>" + 
-            "Name" + element.name + 
-            " , " + 
-            "Role" + element.role + 
-            " , " +             
-            "Company" + element.company + 
-            " , " +             
-            "Date of Graduation" + element.yearOfGraduation + 
+            "Name: " + element.name + 
+            " <br> " + 
+            "Role: " + element.role + 
+            " <br> " +             
+            "Company: " + element.company + 
+            " <br> " +             
+            "Date of Graduation: " + element.yearOfGraduation + 
+
+            ` <button onclick="editGraduate('${element._id}')">Edit Graduate </button>` + 
+            ` <button onclick="deleteGraduate('${element._id}')">Delete Graduate </button>` +
             "</li>" 
         )})
             document.getElementById("results").innerHTML = 
@@ -36,21 +39,63 @@ const renderGrads = () => {
 }
 
 
-function addGraduate(e) { 
-    e.preventDefault(); 
-    console.log("Submit"); 
+function addGraduate(e) {
+    e.preventDefault();
+    console.log("Submit");
+
+    let grad = {
+        name: document.getElementById("name").value,
+        role: document.getElementById("role").value,
+        company: document.getElementById("company").value,
+        yearOfGraduation: document.getElementById("yearOfGraduation").value
+    };
+    console.log(grad);
+
+    let xhrPost = new window.XMLHttpRequest();
+    xhrPost.open("POST", url);
+    xhrPost.setRequestHeader("Content-Type", "application/json");
+    xhrPost.send(JSON.stringify(grad));
+
+}
+
+function editGraduate(id) {
+    console.log('editing open')
 
     let grad = { 
         name: document.getElementById('name').value, 
         role: document.getElementById('role').value, 
         company: document.getElementById('company').value, 
-        yearOfGraduation: document.getElementById('yearOfGraduation').value
-    };
-    console.log(grad);
+        yearOfGraduation: document.getElementById("yearOfGraduation").value
+    }; 
 
-    let xhrPost = new window.XMLHttpRequest();
-    xhrPost.open('Post', url);
-    xhrPost.setRequestHeader('Content-Type', 'application/json');
-    xhrPost.send(JSON.stringify(grad)); 
+    let xhrEdit = new XMLHttpRequest (); 
+    xhrEdit.open("POST", url +`/update/${id}`); 
+    xhrEdit.setRequestHeader('Content-type', 'application/json'); 
+    xhrEdit.onload = () => { 
+        let grads = JSON.parse(xhrEdit.response); 
+        if (xhrEdit.readyState == 4 && xhrEdit.status == "200") { 
+            console.table(grads);
+        } else { 
+            console.table(grads);
+        }
+    }
+    xhrEdit.send(JSON.stringify(grad));
+}
+
+
+function deleteGraduate(id) { 
+    console.log('Delete request');
+
+    let xhrDelete = new XMLHttpRequest(); 
+    xhrDelete.open('DELETE', url + `/${id}`, true); 
+    xhrDelete.onload = function () { 
+        let grads = JSON.parse(xhrDelete.response); 
+        if (xhrDelete.readyState == 4 && xhrDelete.status == "200") { 
+            console.table(grads);
+        } else {
+            console.error(grads);
+        }
+    }
+    xhrDelete.send(null);
 }
 
--- routes/api/grads.js
+++ routes/api/grads.js
@@ -16,20 +16,42 @@ router.get('/', async(req, res) => {
 }); 
 
 router.post('/', async(req, res) => { 
-    console.log(req.body); 
     const grad = new Grad({ 
         name: req.body.name, 
         role: req.body.role, 
         company: req.body.company, 
         yearOfGraduation: req.body.yearOfGraduation
-    }); 
+    });
     try { 
         console.log(grad); 
         const newGrad = await grad.save(); 
-        res.status(201).json(newGraduate);
+        res.status(201).json(newGrad);
     } catch (err) { 
         res.status(400).json({ message: err.message });
     }
 });
+
+router.post('/update/:id', async (req, res) => { 
+    Grad.findById(req.params.id)
+        .then(grad => { 
+            grad.name = req.body.name; 
+            grad.role = req.body.role; 
+            grad.company = req.body.company; 
+            grad.yearOfGraduation = req.body.yearOfGraduation; 
+
+            grad.save()
+                .then(() => res.json('grad updated!'))
+                .catch(err => res.status(400).json('Error: ' + err)); 
+            })
+            .catch(err => res.status(400).json('Error: ' + err)); 
+    });
+
+router.delete('/:id', async(req, res) => {
+    console.log(req.params.id); 
+    Grad.findById(req.params.id)
+        .then(grad => grad.remove().then(res.json({ success: 'true" '})))
+        .then(err => res.status(404).json('Not Found.' )); 
+    console.log('deleted'); 
+})
 
 module.exports = router; 
